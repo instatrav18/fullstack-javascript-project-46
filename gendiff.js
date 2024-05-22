cb4f999a993886f8892ed34595ee59eab0cd1fa3
Geo Sol <instatrav18@yandex.ru>
2024-05-22 22:59:43 +0300

    Step 2: add options and arguments

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,7 @@ import { program } from 'commander';
 program
   .version('0.0.1')
   .helpOption('-h, --help','output usage information')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 program.parse(process.argv)
