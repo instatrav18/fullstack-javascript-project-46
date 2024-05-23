1eaf7e00bb6a17fc8afc989fb2b3bf4f891f191f
Geo Sol <instatrav18@yandex.ru>
2024-05-24 00:24:12 +0300

    Step 3: add spaces in output string and ascinema

--- src/gendiff.js
+++ src/gendiff.js
@@ -7,20 +7,18 @@ const genDiff = (filepath1, filepath2) => {
 
   const obj1 = parse(file1);
   const obj2 = parse(file2);
-
   const obj3 = {...obj1, ...obj2}
-  //console.log(obj3);
-
   const keys = Object.keys(obj3).sort();
+  const spaces = '  ';
 
   //console.log(keys);
   const result = keys.map((item) => {
-    if (Object.hasOwn(obj1, item) && !Object.hasOwn(obj2, item)) return `- ${item}: ${obj1[item]}`;
-    if (!Object.hasOwn(obj1, item) && Object.hasOwn(obj2, item)) return `+ ${item}: ${obj2[item]}`;
+    if (Object.hasOwn(obj1, item) && !Object.hasOwn(obj2, item)) return `${spaces}- ${item}: ${obj1[item]}`;
+    if (!Object.hasOwn(obj1, item) && Object.hasOwn(obj2, item)) return `${spaces}+ ${item}: ${obj2[item]}`;
     if (Object.hasOwn(obj1, item) && Object.hasOwn(obj2, item) && obj1[item] !== obj2[item]) {
-      return `- ${item}: ${obj1[item]}\n+ ${item}: ${obj2[item]}`;
+      return `${spaces}- ${item}: ${obj1[item]}\n${spaces}+ ${item}: ${obj2[item]}`;
     } else {
-      return `  ${item}: ${obj1[item]}`
+      return `${spaces}  ${item}: ${obj1[item]}`
     }
   });
 
